312616eb2f4422cdf2d67f55c227d82b51cf9a51
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2023-09-02 15:11:44 -0300

    Creación GetUser()

--- server/server.py
+++ server/server.py
@@ -4,11 +4,33 @@ from servicio_pb2 import ResponseUser
 import grpc
 from concurrent import futures
 
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+db = psycopg2.connect(
+    user="postgres",
+    password="root",
+    host="localhost",
+    port='5432',
+    database = "chefencasa"
+)
+db.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
+
+cursor = db.cursor();
+
 class ServiceChefEnCasa(ChefEnCasaServicer):
     def GetUser(self, request, context):
-        print(request.userName)
-        print(request.password)
-        return ResponseUser(id = 1, name = "Mauro" , lastName = "Pereyra" , userName = f"{request.userName}")
+        try:
+            query = "SELECT u.id, u.name, u.last_name, u.username from users as u WHERE u.username = '{0}' AND u.password = '{1}'".format(request.userName, request.password)
+            cursor.execute(query)
+            result = cursor.fetchone()
+            if(result is None):
+                return ResponseUser(id=-1)                  
+            else:
+                return ResponseUser(id = result[0], name = result[1], lastName = result[2], userName = result[3])                          
+        except BaseException as error:
+            print(f"Unexpected {error=}, {type(error)=}")
+            return ResponseUser(id=-1)
 
 def start():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
