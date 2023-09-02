e1ee9e3a1dc0e8a9a60bdb46a79257fc34ff462f
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2023-09-02 14:24:10 -0300

    Actualización archivo servicio.proto

--- server/server.py
+++ server/server.py
@@ -1,16 +1,18 @@
-from servicio_pb2_grpc import GreeterServicer, add_GreeterServicer_to_server
-from servicio_pb2 import HelloReply
+from servicio_pb2_grpc import ChefEnCasaServicer, add_ChefEnCasaServicer_to_server
+from servicio_pb2 import ResponseUser
 
 import grpc
 from concurrent import futures
 
-class ServicioGreet(GreeterServicer):
-    def SayHello(self, request, context):
-        return HelloReply(message = (f"Hola {request.name}!"))
+class ServiceChefEnCasa(ChefEnCasaServicer):
+    def GetUser(self, request, context):
+        print(request.userName)
+        print(request.password)
+        return ResponseUser(id = 1, name = "Mauro" , lastName = "Pereyra" , userName = f"{request.userName}")
 
 def start():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    add_GreeterServicer_to_server(ServicioGreet(),server)
+    add_ChefEnCasaServicer_to_server(ServiceChefEnCasa(),server)
     server.add_insecure_port('[::]:50051')
     print("Servidor escuchando en 50051!")
     server.start()
