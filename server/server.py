e8c6c8e5d722e6911c2ad7466bf36c9d20ba7aab
MidoloS <midolo.1912@gmail.com>
2023-09-05 17:25:31 -0300

    recipe searcher working

--- server/server.py
+++ server/server.py
@@ -19,6 +19,109 @@ db.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
 cursor = db.cursor();
 
 class ServiceChefEnCasa(ChefEnCasaServicer):
+    def GetRecipiesByFilters(self, request, context):
+        print(request)
+        print(type(request))
+
+        allRecipes = []
+
+        try:
+            # preparation_time_minutes lower than
+            # title contains
+            query_recipes = """
+                SELECT * FROM recipes as r where r.id_category = {0} and title like '%{1}%' and r.preparation_time_minutes <= {2}
+            """.format(request.category.id, request.title.lower(), request.prepatarionTimeMinutesMax)
+
+            print("catId")
+            print(request.category.id)
+
+            if (request.category.id == 0):
+                query_recipes = """
+                    SELECT * FROM recipes as r where lower(title) like '%{0}%' and r.preparation_time_minutes <= {1}
+                """.format(request.title.lower(), request.prepatarionTimeMinutesMax)
+                if (request.title == "."):
+                    query_recipes = """
+                        SELECT * FROM recipes as r where r.preparation_time_minutes <= {0}
+                    """.format(request.prepatarionTimeMinutesMax)
+
+            print(query_recipes)
+            
+
+            cursor.execute(query_recipes)
+
+            column_names = [desc[0] for desc in cursor.description]
+            print(column_names)
+           
+            for row in cursor.fetchall():
+                print(row)
+
+                query_photos = """
+                    SELECT * FROM recipe_photos as rp WHERE rp.id_recipe = {0}
+                    """.format(row[0])
+                
+                cursor.execute(query_photos)
+                photos = []
+
+                
+
+                for row_photo in cursor.fetchall():
+                    photo = Photo(id = row_photo[0], url = row_photo[1])
+                    photos.append(photo)
+
+
+                query_ingredients = """
+                    SELECT i.id, i.name FROM recipe_ingredients as ri INNER JOIN ingredient as i ON ri.id_ingredient = i.id WHERE ri.id_recipe = {0}
+                    """.format(row[0])
+                
+                cursor.execute(query_ingredients)
+                ingredients = []
+                for row_ingredient in cursor.fetchall():
+                    ingredient = Ingredient(id = row_ingredient[0], name = row_ingredient[1])
+                    ingredients.append(ingredient)
+
+
+                print("ingredients")
+                print(ingredients)
+
+                query_category = """
+                    SELECT c.id, c.name FROM category as c WHERE c.id = {0}
+                    """.format(row[5])
+                cursor.execute(query_category)
+                result_category = cursor.fetchone()
+                category = Category(id = result_category[0], name = result_category[1])
+
+                print(2)
+
+                query_user = """
+                    SELECT u.id, u.name, u.last_name, u.username FROM users as u WHERE u.id = {0}
+                    """.format(row[0])
+                cursor.execute(query_user)
+                result_user = cursor.fetchone()
+                user = User(id = result_user[0], name = result_user[1], userName = result_user[2])
+
+
+
+                print("ingredients")
+                print(ingredients)
+
+                print("user")
+                print(user)
+
+                print("category")
+                print(category)
+
+
+
+                recipe = Reciepe(idReciepe = row[0], title=row[1], description=row[2], photos=photos, ingredients=ingredients, category=category, prepatarionTimeMinutes=row[3], idUser=row[4])
+                allRecipes.append(recipe)
+            return ResponseRecipes(recipes = allRecipes)
+
+        except BaseException as error:
+            print(f"Unexpected {error=}, {type(error)=}")
+            return ResponseRecipes(recipes = allRecipes)
+
+            
+
     
     def GetAllCategorys(self, request, context):
         allCategorys = []
@@ -58,7 +161,8 @@ class ServiceChefEnCasa(ChefEnCasaServicer):
             """
             cursor.execute(query_recipes)
 
-
+            column_names = [desc[0] for desc in cursor.description]
+            print(column_names)
            
             for row in cursor.fetchall():
                 print(row)
@@ -69,6 +173,9 @@ class ServiceChefEnCasa(ChefEnCasaServicer):
                 
                 cursor.execute(query_photos)
                 photos = []
+
+                
+
                 for row_photo in cursor.fetchall():
                     photo = Photo(id = row_photo[0], url = row_photo[1])
                     photos.append(photo)
@@ -84,12 +191,18 @@ class ServiceChefEnCasa(ChefEnCasaServicer):
                     ingredient = Ingredient(id = row_ingredient[0], name = row_ingredient[1])
                     ingredients.append(ingredient)
 
+
+                print("ingredients")
+                print(ingredients)
+
                 query_category = """
                     SELECT c.id, c.name FROM category as c WHERE c.id = {0}
                     """.format(row[5])
                 cursor.execute(query_category)
                 result_category = cursor.fetchone()
                 category = Category(id = result_category[0], name = result_category[1])
+
+                print(2)
 
                 query_user = """
                     SELECT u.id, u.name, u.last_name, u.username FROM users as u WHERE u.id = {0}
@@ -99,10 +212,19 @@ class ServiceChefEnCasa(ChefEnCasaServicer):
                 user = User(id = result_user[0], name = result_user[1], userName = result_user[2])
 
 
+
+                print("ingredients")
                 print(ingredients)
 
+                print("user")
+                print(user)
 
-                recipe = Reciepe(idReciepe = row[0], title=row[1], description=row[2], photos=photos, ingredients=ingredients, category=category, prepatarionTimeMinutes=row[3], user=user)
+                print("category")
+                print(category)
+
+
+
+                recipe = Reciepe(idReciepe = row[0], title=row[1], description=row[2], photos=photos, ingredients=ingredients, category=category, prepatarionTimeMinutes=row[3], idUser=row[4])
                 allRecipes.append(recipe)
             return ResponseRecipes(recipes = allRecipes)
 
